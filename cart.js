7644fd29810e9c2e5e1584784dea02433ae9fbaf
Mahmoud-A-Yehia <mahmoud.am.yehia@gmail.com>
2024-07-11 01:10:39 +0300

    first

--- cart.js
+++ cart.js
@@ -35,7 +35,7 @@
 //         const cell5 = newRow.insertCell(4);
 //         const cell6 = newRow.insertCell(5);
 //         const cell7 = newRow.insertCell(6);
-        
+
 //         cell1.innerHTML = `<img src="${dat[i].thumbnail}" alt="${dat[i].title}">`;
 //         cell2.textContent = dat[i].title;
 //         cell3.textContent = `$${dat[i].price}`;
@@ -52,42 +52,42 @@
 
 
 
-var change= [];
+var change = [];
 
 
 
-function cart(data){
-    var dat =data.products;
-    var table = document.getElementById("tbody")
-    table.innerHTML="";
-    //console.log(table.innerHTML)
-    for (let i = 0; i < dat.length; i++) {
+function cart(data) {
+  var dat = data.products;
+  var table = document.getElementById("tbody")
+  table.innerHTML = "";
+  //console.log(table.innerHTML)
+  for (let i = 0; i < dat.length; i++) {
     //    const element = array[i];
-    if(dat[i].quantity>0){
-        const newRow = table.insertRow();
+    if (dat[i].quantity > 0) {
+      const newRow = table.insertRow();
 
-        const cell1 = newRow.insertCell(0);
-        const cell2 = newRow.insertCell(1);
-        const cell3 = newRow.insertCell(2);
-        const cell4 = newRow.insertCell(3);
-        const cell5 = newRow.insertCell(4);
-        const cell6 = newRow.insertCell(5);
-        const cell7 = newRow.insertCell(6);
-        
-        cell1.innerHTML = `<img src="${dat[i].thumbnail}" alt="${dat[i].title}">`;
-        cell2.textContent = dat[i].title;
-        cell3.textContent = `$${dat[i].price}`;
-        cell4.innerHTML = `<input type="number" value="${dat[i].quantity}" min="1" onchange="updatequantity(this,${dat[i].id},${data.id})">`;
-        cell5.textContent = '$'+dat[i].total;
-        if(dat[i].discountedTotal==undefined){
+      const cell1 = newRow.insertCell(0);
+      const cell2 = newRow.insertCell(1);
+      const cell3 = newRow.insertCell(2);
+      const cell4 = newRow.insertCell(3);
+      const cell5 = newRow.insertCell(4);
+      const cell6 = newRow.insertCell(5);
+      const cell7 = newRow.insertCell(6);
+
+      cell1.innerHTML = `<img src="${dat[i].thumbnail}" alt="${dat[i].title}">`;
+      cell2.textContent = dat[i].title;
+      cell3.textContent = `$${dat[i].price}`;
+      cell4.innerHTML = `<input type="number" value="${dat[i].quantity}" min="1" onchange="updatequantity(this,${dat[i].id},${data.id})">`;
+      cell5.textContent = '$' + dat[i].total;
+      if (dat[i].discountedTotal == undefined) {
         cell6.textContent = `$${dat[i].discountedPrice}`;
-        }else{
+      } else {
         cell6.textContent = `$${dat[i].discountedTotal}`;
-        }
-        cell7.innerHTML = `<button class="remove" onclick="removeItem(${dat[i].id},${data.id})">Remove</button>`;
+      }
+      cell7.innerHTML = `<button class="remove" onclick="removeItem(${dat[i].id},${data.id})">Remove</button>`;
     }
-    }
-    document.getElementById("cart-total").innerText=data.discountedTotal;
+  }
+  document.getElementById("cart-total").innerText = data.discountedTotal;
 }
 
 
@@ -95,10 +95,10 @@ function cart(data){
 
 
 fetch('https://dummyjson.com/carts/user/6')
-.then(res => res.json())
-.then(data => {
-cart(data.carts[0]);
-} );
+  .then(res => res.json())
+  .then(data => {
+    cart(data.carts[0]);
+  });
 
 
 
@@ -114,52 +114,52 @@ cart(data.carts[0]);
 
 
 
-function updatequantity(quan,idd,cid){
-    // console.log(quan.value)
-    // console.log(idd)
-change.push({
+function updatequantity(quan, idd, cid) {
+  // console.log(quan.value)
+  // console.log(idd)
+  change.push({
     id: idd,
     quantity: quan.value,
   });
-fetch('https://dummyjson.com/carts/'+cid,{
-    method: 'PUT', 
+  fetch('https://dummyjson.com/carts/' + cid, {
+    method: 'PUT',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
-      merge: true, 
+      merge: true,
       products: change
     })
   })
-  .then(res => res.json())
-  .then(data => {
-    cart(data);
-//    console.log(data);
-    } );
+    .then(res => res.json())
+    .then(data => {
+      cart(data);
+      //    console.log(data);
+    });
 }
 
 
 
 
 
-function removeItem(idd,cid){
-    // console.log(idd)
-    change.push({
-        id: idd,
-        quantity: 0,
-      });
+function removeItem(idd, cid) {
+  // console.log(idd)
+  change.push({
+    id: idd,
+    quantity: 0,
+  });
 
-fetch('https://dummyjson.com/carts/'+cid,{
-    method: 'PUT', 
+  fetch('https://dummyjson.com/carts/' + cid, {
+    method: 'PUT',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
-      merge: true, 
+      merge: true,
       products: change
     })
   })
-  .then(res => res.json())
-  .then(data => {
-    cart(data);
-//    console.log(data);
-    } );
+    .then(res => res.json())
+    .then(data => {
+      cart(data);
+      //    console.log(data);
+    });
 }
 
 
@@ -169,34 +169,34 @@ fetch('https://dummyjson.com/carts/'+cid,{
 
 
 fetch('https://dummyjson.com/carts/1')
-.then(res => res.json())
-.then(console.log);
+  .then(res => res.json())
+  .then(console.log);
 
 
 
 
 /* adding products in cart with id 1 */
 fetch('https://dummyjson.com/carts/1', {
-    method: 'PUT', /* or PATCH */
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      merge: true, // this will include existing products in the cart
-      products: [
-        {
-          id: 1,
-          quantity: 1,
-        },
-        {
-            id: 2,
-            quantity: 2,
-          },
-          {
-            id: 1,
-            quantity: 5,
-          },
-      ]
-    })
+  method: 'PUT', /* or PATCH */
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({
+    merge: true, // this will include existing products in the cart
+    products: [
+      {
+        id: 1,
+        quantity: 1,
+      },
+      {
+        id: 2,
+        quantity: 2,
+      },
+      {
+        id: 1,
+        quantity: 5,
+      },
+    ]
   })
+})
   .then(res => res.json())
   .then(console.log);
 
